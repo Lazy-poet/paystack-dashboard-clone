38276f7b222e85f2e4cd8e381817b733b51b6b9e
Emmanuel Elegbede <emmanuelhemarxyll@gmail.com>
2021-03-10 03:05:16 +0000

    modified files

--- src/icons.js
+++
@@ -1,8 +0,0 @@
-import { Vue } from 'vue';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
-import { faChevronCircleDown } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faChevronCircleDown)
-
-Vue.component('fa-icon', FontAwesomeIcon)
